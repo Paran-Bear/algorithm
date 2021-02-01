238a8d22be1092e5de7834a50cde397b992eda04
Paran-Bear <51732452+Paran-Bear@users.noreply.github.com>
2021-02-02 04:44:02 +0900

    Update 큰수의법칙.py

--- 큰수의법칙.py
+++ 큰수의법칙.py
@@ -1,32 +1,50 @@
-n, m = map(int, input().split())
-data=[]
-for i in range(n):
-  data.append(list(map(int,input().split())))
-print(data)
-  
-def my_answer(data):
+print("start")
+n, m, k = map(int, input().split())
+#n,m,k의 값이 맵에 공백으로 구분되어 저장
+
+data = list(map(int, input().split()))
+#n의 배열값이 맵으로 저장됨. 공백으로 구분
+data.sort()
+#배열의 값을 오름차순으로 정렬.
+first = data[n - 1]
+#이미 정렬된 데이터의 마지막 값(큰수)
+second = data[n - 2]
+#뒤에서 두번쨰 값(두번째로 큰수)
+
+
+def my_answer(m, k):
+    count = 0
+    result = 0
+    while (True):
+        for j in range(k):
+            if (count != m):
+                result += first
+                count += 1
+            else:
+                break
+        if (count != m):
+            result += second
+            count += 1
+        else:
+            break
+    return result
+#나의 답은 단순히 과정만 생각하여 도출한 코드
+
+def answer(m,k):
+  count=int(m/(k+1))*k
+  count+=m%(k+1)
+
   result=0
-  for i in range(n):
-      data[i].sort()
-      if(int(result)<int(data[i][0])):
-        result=data[i][0]
-  #반복문에서 정렬을 한다음 비교문을 통해 정답을 도출
+  result+=(count)*first
+  result+=(m-count)*second
   return result
+#책에서 제시한 답은 반복되는 수열을 보고,
+#반복되는 수열을 구하고 나머지를 따로 계산함.
+#시간 복잡도면에서 더 이득이 됨.
 
 
-def answer(data):
-  result=0
-  for i in range(n):
-    min_value=min(data[i])
-    result=max(result,min_value)
-  return result
-  #각 배열의 최솟값을 알면 되는 문제이다. 
-  #min 함수로 최솟값만 찾고, max로 각 배열의 최솟값중 최댓값을 찾음.
-print("my_answer->")
-print(my_answer(data))
-print("answer->")
-print(answer(data))
-#print(data[0][m-1])
-
-
+print("my answer")
+print(answer(m,k))
+print("answer")
+print(answer(m, k))
 
